1495d75b7462868735049008b084fc115bf37f86
ryunji1990@gmail.com <ryunji1990@gmail.com>
2024-07-03 17:28:35 +0900

    청크 사이즈 조절 수정 중

--- src/database/chatDB/insert_data.py
+++ src/database/chatDB/insert_data.py
@@ -22,8 +22,15 @@ def load_vector_db():
 def load_csv_data():
     
     try:
-        df = pd.read_csv('crawling2024.csv')
-        documents = [Document(page_content=row['판례내용'], metadata={"id": str(row['판례일련번호'])}) for _, row in df.iterrows()]
+        df = pd.read_csv('data.csv')
+        documents = []
+        for index, row in df.iterrows():
+            #print(f"Index: {index}")  # 인덱스 출력
+            판례일련번호 = str(row['판례일련번호'])
+            판례내용     = str(row['판례내용'])
+            #print(f"판례일련번호: {판례일련번호}")  # 판례일련번호 출력
+            #print(f"판례내용: {판례내용}")  # 판례일련번호 출력
+            documents.append(Document(page_content=row['판례내용'], metadata={"id": 판례일련번호}))
         print("CSV 데이터 로드 성공")
         return documents
     except Exception as e:
@@ -38,18 +45,37 @@ def tiktoken_len(text):
 
 # 2. 텍스트 청크 처리 함수
 def get_text_chunks(documents):
-    try:
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=100,   #100 150 
-            chunk_overlap=10, #10
-            length_function=tiktoken_len
-        )
-        chunks = text_splitter.split_documents(documents)
-        print("텍스트 청크 처리 성공")
-        return chunks
-    except Exception as e:
-        print(f"텍스트 청크 처리 실패: {e}")
-        return None
+    
+    all_chunks = []
+    for index, doc in enumerate(documents):
+        content = doc.page_content
+        print("청크 대상 순번 :", index)
+        #print("청크 대상 판례 아이디:", doc.metadata['id'])
+        #print("청크 대상 판례:", doc.page_content)
+
+
+        try:
+            text_splitter = RecursiveCharacterTextSplitter(
+                  chunk_size=100
+                , chunk_overlap=20
+                , length_function=tiktoken_len
+            )
+            # 개별 판례 사례를 청크 처리
+            chunks = text_splitter.split_documents([doc])
+            #print("청크 처리된 결과 :", chunks)
+            for chunk_index, chunk in enumerate(chunks):
+                print("꼭 확인 chunk :", chunk)
+                all_chunks.extend(chunk)
+        except Exception as e:
+            print(f"텍스트 청크 처리 실패 (판례 아이디: {doc.metadata['id']}): {e}")
+    
+    return
+    for index, chunk in enumerate(all_chunks):
+        print("index :", index)
+        print(f"청크 {index} 결과:")
+        print("")  # 청크 사이에 빈 줄 추가
+
+    return all_chunks if all_chunks else None
 
 # 임베딩 모델 생성 함수
 def get_embeddings():
@@ -67,6 +93,7 @@ def get_embeddings():
 
 # 벡터 스토어 생성 및 데이터 저장 함수
 def save_to_vectorstore(text_chunks):
+    
     embeddings = get_embeddings()
     if embeddings is None:
         print("임베딩 모델을 로드할 수 없습니다.")
@@ -79,20 +106,9 @@ def save_to_vectorstore(text_chunks):
 
     try:
         
-        existing_case_ids = set()
-
-        # 모든 문서를 반복하여 가져오기
-        #cursor = collection.find({})
-        #for doc in cursor:
-            #existing_case_ids.add(doc['metadata']['case_id'])  # 이 부분은 collection의 데이터 접근 방법에 따라 수정하세요.
-
         for i, chunk in enumerate(text_chunks):
             
-            #case_id = chunk.metadata['id']
-            #if case_id in existing_case_ids:
-                #print(f"중복된 case_id: {case_id} - 저장되지 않음")
-                #continue  # 중복된 경우 추가하지 않고 넘어갑니다.    
-            
+            print("for문 :", i)
             chunk_id = f"doc_{i}_{chunk.metadata['id']}"
             embedding = embeddings.embed_documents([chunk.page_content])[0]
 
@@ -119,13 +135,12 @@ def main():
         print("CSV 데이터를 로드할 수 없습니다.")
         return
 
-    print("1.documents :", documents)
-
     # 2. 데이터를 작업하기 쉽게 분할하고 묶는다.  
     text_chunks = get_text_chunks(documents)
     if text_chunks is None:
         print("텍스트 청크를 처리할 수 없습니다.")
         return
+
 
     # 3. 벡터 DB로 데이터를 insert 한다.
     vectorstore = save_to_vectorstore(text_chunks)
